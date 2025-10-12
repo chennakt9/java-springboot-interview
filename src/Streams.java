2ed32be29205e3f0c02171246ca737cd840fc577
chennaatdazn <chenna.kesava@dazn.com>
2025-10-12 18:42:38 +0530

    feat: streams

--- src/Streams.java
+++ src/Streams.java
@@ -1,25 +1,90 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+// Ref : https://www.youtube.com/playlist?list=PLFGoYjJG_fqp52WKlmgF4cV72KS9_9tih
 public class Streams {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> list = Arrays.asList(1, 10, 20, 30, 15, 1);
 
-        // 1. Filter: only even numbers
-        List<Integer> evens = numbers.stream()
+        // Question 1: Sum of numbers
+        Integer sum = list.stream().reduce(0, Integer::sum);
+        System.out.println("Sum: " + sum);
+
+        // Question 2: average of numbers
+        double avg = list.stream().mapToInt(Integer::intValue).average().orElse(0);
+        System.out.println("Avg: " + avg);
+
+        // Question 3 : square, filter, avg
+        double avg1 = list
+                .stream()
+                .map((num) -> num * num)
+                .filter((num) -> num > 100)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+
+        System.out.println("Avg1: " + avg1);
+
+        // Question 4 : Even, Odd
+        List<Integer> evens = list
+                .stream()
                 .filter(n -> n % 2 == 0)
                 .toList();
-        System.out.println("Even numbers: " + evens);
 
-        // 2. Map: square each number
-        List<Integer> squares = numbers.stream()
-                .map(n -> n * n)
+        System.out.println("Evens: " + evens);
+
+        // Question 5 : Numbers starts with prefix 2
+        List<Integer> ints2 = list
+                .stream()
+                .map(String::valueOf)
+                .filter(e -> e.startsWith("2"))
+                .map(Integer::valueOf)
                 .toList();
-        System.out.println("Squares: " + squares);
 
-        // 3. Sum: sum of all numbers
-        int sum = numbers.stream()
-                .reduce(0, Integer::sum);
-        System.out.println("Sum: " + sum);
+        System.out.println("ints2: " + ints2);
+
+        // Question 6 : Duplicates in a list
+        Set<Integer> dups = list
+                .stream()
+                .filter(e -> Collections.frequency(list,e) > 1)
+                .collect(Collectors.toSet());
+
+        System.out.println("Dups: " + dups);
+
+        // Question 7 : Max and min
+        int max = list
+                .stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElse(0);
+
+        System.out.println("max: " + max);
+
+        // Question 8: Sort
+        List<Integer> sorted = list
+                .stream()
+                .sorted(Collections.reverseOrder())
+                .toList();
+
+        System.out.println("sorted: " + sorted);
+
+        // Question 9 : Limit
+        int limitSum = list
+                .stream()
+                .limit(5)
+                .mapToInt(Integer::intValue)
+                        .sum();
+
+        System.out.println("limitSum: " + limitSum);
+
+        // Question 10 : Second highest
+        int secondH = list
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(0);
+
+        System.out.println("secondH: " + secondH);
     }
 }
