80acee95ae04edbed9048b534da356437f4e50bf
chennaatdazn <chenna.kesava@dazn.com>
2025-11-24 10:23:47 +0530

    feat: meeting rooms

---
+++ src/lld/MeetingRoom.java
@@ -0,0 +1,90 @@
+package lld;
+
+/*
+    ref : https://youtu.be/5F-3BzCuUS0?si=9tTDLPlUmTzR0WS-
+    ==Requirements==
+    - Users can book any meeting room from (start time to end time) if the room is available.
+    - If a room is not available, it should be shown at that moment only.
+    - All the participants of a meeting should receive a notification related to the same.
+    - Store the history of all the meetings scheduled.
+    - Users can see all the available meeting rooms for a particular time slot ( can be neglected for implementation but is good to discuss)
+
+    User
+    - userId
+    - name
+    - email
+    + getBookings()
+
+    MeetingRoom
+    - roomId
+    - name
+    - capacity
+    + isAvailable(startTime, endTime)
+    + book(booking)
+
+    Booking
+    - bookingId
+    - startTime
+    - endTime
+    - participants : List<User>
+    - meetingRoom : MeetingRoom
+    - selectedFeatures : List<String>
+    + notifyParticipants()
+
+    BookingRequest
+    - startTime
+    - endTime
+    - participants : List<User>
+    - tvRequired : boolean
+    - whiteboardRequired : boolean
+    - acRequired : boolean
+
+    BookingRequestBuilder
+    - startTime
+    - endTime
+    - participants : List<User>
+    - tvRequired
+    - whiteboardRequired
+    - acRequired
+    + setStartTime(start)
+    + setEndTime(end)
+    + addParticipant(user)
+    + requireTV()
+    + requireWhiteboard()
+    + requireAC()
+    + build() : BookingRequest
+
+    RoomFeature <Interface>
+    + getDescription()
+
+    BasicRoom <implements RoomFeature>
+    - meetingRoom
+    + getDescription()
+
+    RoomDecorator <implements RoomFeature>
+    - wrappedRoom : RoomFeature
+    + getDescription()
+
+    TVDecorator <implements RoomFeature>
+    - wrappedRoom
+    + getDescription()
+
+    WhiteboardDecorator <implements RoomFeature>
+        - wrappedRoom
+        + getDescription()
+
+    ACDecorator <implements RoomFeature>
+        - wrappedRoom
+        + getDescription()
+
+    NotificationService
+    - strategies : Map<type, NotificationStrategy>
+    + update(booking)
+
+    note:
+        1. If we have optional features, use builder pattern along with decorator pattern
+        2. Use strategy pattern for notification service
+*/
+
+public class MeetingRoom {
+}
