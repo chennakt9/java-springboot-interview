6e20e142652b9a40a5cef2cd21d5071bebf61b26
chennaatdazn <chenna.kesava@dazn.com>
2025-11-25 15:31:39 +0530

    feat: billing system

---
+++ src/lld/BillingSystem.java
@@ -0,0 +1,41 @@
+package lld;
+
+/*
+    ref : https://chatgpt.com/share/69257d85-9dc0-800e-a9e1-18046de5be72
+
+Core
+
+Customer
+Product
+
+Billing (Composite)
+
+LineItem
+Invoice
+
+Factories
+
+InvoiceFactory
+PaymentFactory
+
+Strategy
+
+DiscountStrategy
+NoDiscountStrategy
+PercentageDiscountStrategy
+FlatDiscountStrategy
+
+Observer
+
+InvoiceObserver
+EmailNotifier
+SMSNotifier
+
+Singleton
+BillingService
+
+Payments
+Payment
+*/
+public class BillingSystem {
+}
